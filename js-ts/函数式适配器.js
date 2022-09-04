4b68b084eaeb180bdec363941f62c7d17edcf439
yuhj <snowwolfjay@foxmail.com>
2022-09-04 11:08:20 +0800

    基本的中文程序适配器

--- js-ts/函数式适配器.js
+++ js-ts/函数式适配器.js
@@ -33,11 +33,25 @@ global.打印 = (...参数组) => {
 }
 
 
-global.状态 = (状态) => {
+global.程序 = () => {
+    const 状态 = {}
     return {
-        状态,
+        值: 状态,
         执行(指令) {
-            指令(状态)
+            指令(状态, this)
+            return this
+        },
+        令(键, 值) {
+            const 路径 = 键.split(".")
+            let 对象 = 状态
+            while (路径.length > 1) {
+                const 临时 = 路径.shift()
+                if (!对象[临时]) {
+                    对象[临时] = {}
+                }
+                对象 = 对象[临时]
+            }
+            对象[路径[0]] = 值
             return this
         }
     }
