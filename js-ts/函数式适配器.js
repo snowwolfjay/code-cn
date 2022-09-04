5b53a44e46e36cdfe6c48864dc035cfcb18f6022
yuhj <snowwolfjay@foxmail.com>
2022-09-04 10:54:20 +0800

    函数式编程和状态化传参

--- js-ts/函数式适配器.js
+++ js-ts/函数式适配器.js
@@ -31,3 +31,17 @@ global.假如 = (条件) => {
 global.打印 = (...参数组) => {
     console.log(...参数组)
 }
+
+
+global.状态 = (状态) => {
+    return {
+        状态,
+        执行(指令) {
+            指令(状态)
+            return this
+        }
+    }
+}
+
+global.函数 = (定义,) => new Function('状态', 定义)
+global.参数 = global.函数
