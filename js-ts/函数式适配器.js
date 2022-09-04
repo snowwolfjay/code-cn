d3a75901e3d30bbaa68bddac8591ae17f7f524a0
yuhj <snowwolfjay@foxmail.com>
2022-09-04 11:39:53 +0800

    程序支持指令声明转为函数，新增程序和全局上下文参数

--- js-ts/函数式适配器.js
+++ js-ts/函数式适配器.js
@@ -35,11 +35,15 @@ global.打印 = (...参数组) => {
 
 global.程序 = () => {
     const 状态 = {}
-    return {
+    const 环境 = global;
+    const 程序 = {
         状态,
         执行(指令) {
-            指令(状态, this)
-            return this
+            if (typeof 指令 === "string") {
+                指令 = 函数(指令)
+            }
+            指令(状态, 程序, global)
+            return 程序
         },
         令(键, 值) {
             const 路径 = 键.split(".")
@@ -52,41 +56,42 @@ global.程序 = () => {
                 对象 = 对象[临时]
             }
             对象[路径[0]] = 值
-            return this
+            return 程序
         },
-        如果(条件函数) {
+        如果(条件声明) {
             const 甲 = []
-            const 条件 = 条件函数(状态)
+            const 条件 = 函数("return " + 条件声明)(状态, 程序, 环境)
             const 条件对象 = {
-                那么: (函数) => {
+                那么: (函数声明) => {
                     if (条件) {
-                        函数()
+                        函数(函数声明)(状态, 程序, 环境)
                     }
                     甲.push(1)
                     if (甲.length === 2) {
-                        return this
+                        return 程序
                     }
                     return 条件对象
                 },
-                否则: (函数) => {
+                否则: (函数声明) => {
                     if (!条件) {
-                        函数()
+                        函数(函数声明)(状态, 程序, 环境)
                     }
                     甲.push(1)
                     if (甲.length === 2) {
-                        return this
+                        return 程序
                     }
                     return 条件对象
                 }
             }
             return 条件对象
 
+        },
+        修改状态(函数字符串) {
+            return 程序.执行(函数(函数字符串))
         }
     }
+    return 程序
 }
 
-global.函数 = (定义,) => new Function('状态',
-    定义
-        .replace("返回", "return")
-)
+global.函数 = (定义) => new Function('状态', '程序', '环境', 定义)
 global.参数 = global.函数
